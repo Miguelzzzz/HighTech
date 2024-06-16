e13274e51a2bf2ec5b7d4b61158bc81180e734aa
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2024-06-16 15:16:57 -0300

    edicao
    trello

--- model/bdlogin.sql
+++ model/bdlogin.sql
@@ -2,9 +2,10 @@ create database bdlogin;
 use bdlogin;
 
 create table tblogin (
-email varchar(320) not null unique primary key,
-senha varchar(35) not null,
-usuario varchar(25) not null unique
+id int auto_increment primary key,
+usuario varchar(25) not null,
+email varchar(320) not null unique,
+senha varchar(35) not null
 );
 -- select * from tblogin;
 
